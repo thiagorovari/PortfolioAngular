c0d1f730986a2295aeac61fdab03ba955912eb27
Thiago Rovari <thiagorovari@gmail.com>
2025-01-28 10:26:15 -0300

    Iniciando alteracoes no header

--- portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
