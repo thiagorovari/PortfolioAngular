2cb8d546354b0c2b760efc12f217873804aa86ce
Thiago Rovari <thiagorovari@gmail.com>
2025-01-28 17:26:50 -0300

    Knowledge HTML e criação da componente projects

--- portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
+import { ExperienceComponent } from '../../components/experience/experience.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, KnowledgeComponent],
+  imports: [HeaderComponent, KnowledgeComponent, ExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
