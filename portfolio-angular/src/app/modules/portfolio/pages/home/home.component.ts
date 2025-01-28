8fc7b9b0c6391d9bba3a0a9b53447fd07d63ea68
Thiago Rovari <thiagorovari@gmail.com>
2025-01-28 17:44:00 -0300

    Remoçao do node_modules errado. Início da componente projects

--- portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 import { ExperienceComponent } from '../../components/experience/experience.component';
+import { ProjectsComponent } from '../../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, KnowledgeComponent, ExperienceComponent],
+  imports: [HeaderComponent, KnowledgeComponent, ExperienceComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
