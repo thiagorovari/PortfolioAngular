1e8175c7aa147ebe3b5044bf87dc44c998f9a71f
Thiago Rovari <thiagorovari@gmail.com>
2025-01-28 17:13:58 -0300

    Knowledge HTML

---
+++ portfolio-angular/src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,33 @@
+import { Component, signal } from '@angular/core';
+import { IKnowledge } from '../../interface/IKnowledge.interface';
+
+@Component({
+  selector: 'app-knowledge',
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: 'assets/icons/knowledge/html5.svg',
+      alt: 'Ícone de conhecimento de html5',
+    },
+    {
+      src: 'assets/icons/knowledge/css3.svg',
+      alt: 'Ícone de conhecimento de css3',
+    },
+    {
+      src: 'assets/icons/knowledge/javascript.svg',
+      alt: 'Ícone de conhecimento de javascript',
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Ícone de conhecimento de angular',
+    },
+    {
+      src: 'assets/icons/knowledge/nodejs.svg',
+      alt: 'Ícone de conhecimento de nodejs',
+    },
+  ]);
+}
--- portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio-angular/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { KnowledgeComponent } from '../../components/knowledge/knowledge.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, KnowledgeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
